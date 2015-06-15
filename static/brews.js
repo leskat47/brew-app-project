a2024ded135e1ba47e9b04b313b68a25b196700f
Leslie Castellanos <lesliekate@gmail.com>
2015-06-14 17:34:25 -0700

    Made add to calendar feature more flexible. Made timer buttons color when the respective timer is running.

--- static/brews.js
+++ static/brews.js
@@ -38,6 +38,8 @@ $('.glyphicon').click(function(){
 // TIMER:
 // timer box toggle and drag:
 	$(".timer").click(function(evt){
+		$(".timer").css("background-color", "#ffffff").text("Start a Timer");
+		$(this).css("background-color", "#e5f4c0;").text("Timer Running");
 		timer.start($(this).val() * 60000);
 	});
 	$("#showtimer").hide();
@@ -80,78 +82,113 @@ $(".stop").on('click', function() {
 
 
 // CALENDAR:
-	$("#c-days").change( function(){
-		var date = new Date();
-		var days = parseInt($("#c-days").val());
-		date.setDate(date.getDate() + days);
+$("#rem-display").hide()
+$("input[name=rem]:radio").change( function(){
+	$("#get-inst").val('');
+	$("#c-days").val('');
 
-		year = date.getFullYear();
-		month = date.getMonth() + 1;
-		if (month < 10) {
-			month = "0" + month;
-		}
-		day = date.getDate();
-		if (day < 10) {
-			day = "0" + day;
-		}
-		newdate = year + "-" + month + "-" + day + " 12:00:00";
+	$("#rem-display").show()
+	if (this.value == "gravity") {
+		$("#cal_title").text("Check beer gravity");
+		$("#cal_desc").text("Check gravity for {{ recipe }}");
+		$("#prompt").html('Gravity instructions: <input type="text" id="get-inst"><br>');
+	}
+	else if (this.value == "add-ing") {
+		$("#cal_title").text("Add next brew ingredient");
+		$("#cal_desc").text("Add next brew ingredient");
+		$("#prompt").html('Ingredient reminder: <input type="text" id="get-inst"><br>');
 
-		$("#date_start").text(newdate);
-		$("#date_end").text(newdate);
-		$("#g-days").show();
+	}
+	else if (this.value == "temperature") {	
+		$("#cal_title").text("Check beer temperature");
+		$("#cal_desc").text("Check beer temperature");
+		$("#prompt").html('Temperature instruction: <input type="text" id="get-inst"><br>');
 
-		(function () {
-		            if (window.addtocalendar)if(typeof window.addtocalendar.start == "function")return;
-		            if (window.ifaddtocalendar == undefined) { window.ifaddtocalendar = 1;
-		                var d = document, s = d.createElement('script'), g = 'getElementsByTagName'; 
-		                s.type = 'text/javascript';s.charset = 'UTF-8';s.async = true;
-		                s.src = ('https:' == window.location.protocol ? 'https' : 'http')+'://addtocalendar.com/atc/1.5/atc.min.js';
-		                var h = d[g]('body')[0]; console.log(h); h.appendChild(s);}})();
+	}
+	else if (this.value == "reminder") {
+		$("#cal_title").text("Beer reminder");
+		$("#cal_desc").text("Beer reminder");
+		$("#prompt").html('Other instructions: <input type="text" id="get-inst"><br>');
+	}
+});
+$("#get-inst").change( function(){
+	$("#cal_desc").text($("#get-inst").val());
 
-	});
-	$("#reminder").change(function(){
-		next_step = $("#reminder").val();
-		$("#cal_title").text(next_step);
-		$("#cal_desc").text(next_step);
-	})
+});
 
-	$("#r-days").change( function(){
-		var date = new Date();
-		var days = parseInt($("#r-days").val());
-		date.setDate(date.getDate() + days);
+$("#c-days").change( function(){
+	console.log("change")
+	var date = new Date();
+	var days = parseInt($("#c-days").val());
+	date.setDate(date.getDate() + days);
 
-		year = date.getFullYear();
-		month = date.getMonth() + 1;
-		if (month < 10) {
-			month = "0" + month;
-		}
-		day = date.getDate();
-		if (day < 10) {
-			day = "0" + day;
-		}
-		newdate = year + "-" + month + "-" + day + " 12:00:00";
-		$("#r_date_start").text(newdate);
-		$("#r_date_end").text(newdate);
-		$("#cal-reminder").show();
+	year = date.getFullYear();
+	month = date.getMonth() + 1;
+	if (month < 10) {
+		month = "0" + month;
+	}
+	day = date.getDate();
+	if (day < 10) {
+		day = "0" + day;
+	}
+	newdate = year + "-" + month + "-" + day + " 12:00:00";
 
-		(function () {
-		            if (window.addtocalendar)if(typeof window.addtocalendar.start == "function")return;
-		            if (window.ifaddtocalendar == undefined) { window.ifaddtocalendar = 1;
-		                var d = document, s = d.createElement('script'), g = 'getElementsByTagName'; 
-		                s.type = 'text/javascript';s.charset = 'UTF-8';s.async = true;
-		                s.src = ('https:' == window.location.protocol ? 'https' : 'http')+'://addtocalendar.com/atc/1.5/atc.min.js';
-		                var h = d[g]('body')[0]; console.log(h); h.appendChild(s);}})();
-	});
-	// ABV:
-	$("#final_gravity").change( function(){
-		var fg = parseFloat($("#final_gravity").val());
-		if ($("#orig_gravity").val() > 0) {
-			var og = parseFloat($("#orig_gravity").val());
-			var abv  = round(((og - fg) * 131), 2);
-			$("#abv").text(abv);
-			$("#abv").html(abv);
-		}
-	});
+	$("#date_start").text(newdate);
+	$("#date_end").text(newdate);
+	$("#g-days").show();
+
+	(function () {
+			if (window.addtocalendar)if(typeof window.addtocalendar.start == "function")return;
+            if (window.ifaddtocalendar == undefined) { window.ifaddtocalendar = 1;
+                var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+                s.type = 'text/javascript';s.charset = 'UTF-8';s.async = true;
+                s.src = ('https:' == window.location.protocol ? 'https' : 'http')+'://addtocalendar.com/atc/1.5/atc.min.js';
+                var h = d[g]('body')[0];h.appendChild(s); }})();
+
+});
+$("#reminder").change(function(){
+	next_step = $("#reminder").val();
+	$("#cal_title").text(next_step);
+	$("#cal_desc").text(next_step);
+})
+
+$("#r-days").change( function(){
+	var date = new Date();
+	var days = parseInt($("#r-days").val());
+	date.setDate(date.getDate() + days);
+
+	year = date.getFullYear();
+	month = date.getMonth() + 1;
+	if (month < 10) {
+		month = "0" + month;
+	}
+	day = date.getDate();
+	if (day < 10) {
+		day = "0" + day;
+	}
+	newdate = year + "-" + month + "-" + day + " 12:00:00";
+	$("#r_date_start").text(newdate);
+	$("#r_date_end").text(newdate);
+	$("#cal-reminder").show();
+
+	(function () {
+	            if (window.addtocalendar)if(typeof window.addtocalendar.start == "function")return;
+	            if (window.ifaddtocalendar == undefined) { window.ifaddtocalendar = 1;
+	                var d = document, s = d.createElement('script'), g = 'getElementById'; 
+	                s.type = 'text/javascript';s.charset = 'UTF-8';s.async = true;
+	                s.src = ('https:' == window.location.protocol ? 'https' : 'http')+'://addtocalendar.com/atc/1.5/atc.min.js';
+	                var h = d[g]('special_rem'); console.log(h); h.appendChild(s);}})();
+});
+// ABV:
+$("#final_gravity").change( function(){
+	var fg = parseFloat($("#final_gravity").val());
+	if ($("#orig_gravity").val() > 0) {
+		var og = parseFloat($("#orig_gravity").val());
+		var abv  = round(((og - fg) * 131), 2);
+		$("#abv").text(abv);
+		$("#abv").html(abv);
+	}
+});
 
 });
 
