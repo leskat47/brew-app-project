2e310cd9909d79fee6abaf1c25a7ba8617ee76de
Robert Chapman <blargony@users.noreply.github.com>
2015-07-03 23:39:20 -0700

    Added a simple view test.  Get/Post data to a URL, in this case the login/logout pages.

--- test.py
+++ test.py
@@ -29,6 +29,9 @@ class TestCase(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
+    # ==============================================================================
+    # Testing User Model
+    # ==============================================================================
     def test_user(self):
         # Make a user
         u = User(first_name='John', last_name='Smith', email='jsmith@example.com', username='jsmith', password='test')
@@ -49,6 +52,39 @@ class TestCase(unittest.TestCase):
         with self.assertRaises(exc.IntegrityError):
             db.session.commit()
         db.session.rollback()
+
+
+    # ==============================================================================
+    # Testing Login/Logout Views
+    # ==============================================================================
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
+    def test_login_logout(self):
+        u = User(first_name='John', last_name='Smith', email='jsmith@example.com', username='jsmith', password='test')
+        db.session.add(u)
+        db.session.commit()
+
+        rv = self.login('jsmith', 'test')
+        self.assertTrue('Logout' in rv.data)
+        rv = self.logout()
+        self.assertTrue('Login' in rv.data)
+
+        rv = self.login('jsmith', 'testb')
+        self.assertTrue('User name and password do not match' in rv.data)
+
+        rv = self.login('jsmithc', 'test')
+        self.assertTrue('This username is not registered - please create an account' in rv.data)
+
+    # ==============================================================================
+    # Testing Login/Logout Views
+    # ==============================================================================
 
 
 if __name__ == '__main__':
