8a6f1a605cc95c1a4235528132b667bc7b6e1d0f
Leslie Castellanos <lesliekate@gmail.com>
2015-07-05 22:19:52 -0700

    Cleaned up spacing

--- static/brews.js
+++ static/brews.js
@@ -143,7 +143,7 @@ $("#c-days").change( function(){
 
 	(function () {
 			if (window.addtocalendar)if(typeof window.addtocalendar.start == "function")return;
-            window.ifaddtocalendar = 1;
+             	window.ifaddtocalendar = 1;
                 var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
                 s.type = 'text/javascript';s.charset = 'UTF-8';s.async = true;
                 s.src = ('https:' == window.location.protocol ? 'https' : 'http')+'://addtocalendar.com/atc/1.5/atc.min.js';
